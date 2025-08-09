216e2610592d04859559c623534fce34ac1afadc
KristaDeimos <kristavoitishkina@yandex.ru>
2025-08-09 18:36:15 +0300

    Create redirect_page_locators.py

---
+++ locators/redirect_page_locators.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+SCOOTER_LOGO = (By.XPATH, '//a[contains(@class, "Header_LogoScooter")]')
+
+YANDEX_LOGO = (By.XPATH, '//a[contains(@class, "Header_LogoYandex")]')
+
+DZEN_NEWS_TEXT = (By.XPATH, '//div[@data-testid="floor-title-text" and text()="Новости"]')
